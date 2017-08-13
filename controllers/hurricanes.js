ec279c3908876a189535abd8f24dda852b9f5cb6
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-08-12 19:26:37 -0700

    Change mongo query from aggregate() to find(), Add between dates

--- controllers/hurricanes.js
+++ controllers/hurricanes.js
@@ -3,6 +3,50 @@ const assert = require('assert');
 
 const url = 'mongodb://localhost:27017/disaster';
 
+
+function singleDates(year, month, day){
+  let query = {}
+
+  if (year) { query["properties.year"] = parseInt(year) }
+  if (month) { query["properties.month"] = parseInt(month) }
+  if (day) { query["properties.day"] = parseInt(day) }
+
+  return query
+}
+
+function betweenDates(yearStart, yearEnd, monthStart, monthEnd, dayStart, dayEnd){
+  let query = {}
+
+  if (yearStart) {
+    query["properties.year"] = {}
+    query["properties.year"]["$gte"] = parseInt(yearStart)
+  }
+  if (yearEnd) {
+    if(query["properties.year"] === undefined) {query["properties.year"] = {} }
+    query["properties.year"]["$lte"] = parseInt(yearEnd)
+  }
+
+  if (monthStart) {
+    query["properties.month"] = {}
+    query["properties.month"]["$gte"] = parseInt(monthStart)
+  }
+  if (monthEnd) {
+    if(query["properties.month"] === undefined) {query["properties.month"] = {} }
+    query["properties.month"]["lte"] = parseInt(monthEnd)
+  }
+
+  if (dayStart) {
+    query["properties.day"] = {}
+    query["properties.day"]["$gte"] = parseInt(dayStart)
+  }
+  if (dayEnd) {
+    if(query["properties.day"] === undefined) {query["properties.day"] = {} }
+    query["properties.day"]["$lte"] = parseInt(dayEnd)
+  }
+
+  return query
+}
+
 module.exports = {
   // URL/api/hurricanes/
   // URL/api/hurricanes?year=2000&month=06&day=23
@@ -18,48 +62,34 @@ module.exports = {
       year,
       month,
       day,
-      limit
+      yearStart,
+      yearEnd,
+      monthStart,
+      monthEnd,
+      dayStart,
+      dayEnd,
+      limit = 0
     } = req.query
 
-    /* $match */
-    let matchQuery = []
+    let query
 
-    if (year) { matchQuery.push({["properties.year"]: parseInt(year)}) }
-    if (month) { matchQuery.push({["properties.month"]: parseInt(month)}) }
-    if (day) { matchQuery.push({["properties.day"]: parseInt(day)}) }
-
-    if(matchQuery.length > 1){
-      matchQuery = {$and: matchQuery}
-    }else{
-      matchQuery = matchQuery[0]
+    if((year||month||day) && (yearStart||yearEnd||monthStart||monthEnd||dayStart||dayEnd)){
+      res.send('Cannot query on specified and between dates.')
+    } else if (year || month || day) {
+      query = singleDates(year, month, day)
+    } else if (yearStart||yearEnd||monthStart||monthEnd||dayStart||dayEnd) {
+      query = betweenDates(yearStart, yearEnd, monthStart, monthEnd, dayStart, dayEnd)
     }
-
-    if(matchQuery !== undefined){matchQuery = {$match: matchQuery}}
-
-    /* $limit */
-
-    let limitQuery = {}
-
-    if(limit) { limitQuery["$limit"] = parseInt(limit) }
-
-    /**/
-
-    // Creates an array to aggregate on.
-    // Requires that the queries aren't empty, are defined, etc...
-    aggregateArr = []
-    if(matchQuery !== undefined){ aggregateArr.push(matchQuery)}
-    if(Object.keys(limitQuery).length > 0){ aggregateArr.push(limitQuery)}
 
     MongoClient.connect(url, (err, db) => {
       assert.equal(err, null)
 
-      db.collection('hurricanes').aggregate(
-        aggregateArr,
-      (err, result) => {
+      db.collection('hurricanes').find(query).limit(parseInt(limit))
+        .toArray((err, result) => {
         assert.equal(err, null)
 
         db.close()
-        res.json(result)
+        res.send(result)
       })
     })
   }
