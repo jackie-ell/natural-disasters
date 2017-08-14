9828a741f5362e0ad8bc000c3dbd816fd1625760
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-08-13 19:51:42 -0700

    Add query test area on landing page

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,3 +1,36 @@
+const q = (target) => { return document.querySelector(target) }
+
+const getData = (query) => {
+  return fetch(query)
+          .then(res => res.json())
+
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const testBtn = q('#api-test-btn')
+  const testOutput = q('#api-test-output')
+
+  const defaultQuery = 'http://localhost:3001/api/hurricanes?year=2011&month=6&day=22'
+
+  const setOutput = (query) => {
+    getData(query)
+      .then(json => JSON.stringify(json,null,2))
+      .then(json => testOutput.innerHTML = json)
+  }
+
+  setOutput(defaultQuery)
+
+  testBtn.addEventListener('click', event => {
+    event.preventDefault()
+
+    let query = q('#api-test').value
+
+    setOutput(query)
+  })
+})
+
 // let map
 // let socket = io.connect()
 // let result
