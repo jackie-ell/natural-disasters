e058945c85e0b346a8bc61933ab602589853da79
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-08-15 14:23:12 -0700

    - Clean up main.js - Add cors package - Add cors to api/hurricane route

--- routes/api/hurricanes.js
+++ routes/api/hurricanes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const cors = require('cors')
 const {index} = require('../../controllers/hurricanes')
 
-router.get('/', index)
+router.get('/', cors(), index)
 
 module.exports = router
