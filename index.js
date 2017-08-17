68cec592f6ea948d024aee8109f3605c81eb230b
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-08-17 15:38:21 -0700

    Cleaup remove body-parser package, move nodemon to dev dependency

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
-// const bodyParser = require('body-parser')
 
 // ROUTE DIRECTORIES
 const home = require('./routes/home')
