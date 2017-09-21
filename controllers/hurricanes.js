7ffce38e5640482cb0b7a6d6a136d1e6389c321a
Jackie Ellenberger <jackie.ellenberger@gmail.com>
2017-09-21 12:28:10 -0700

    Move redudant request/query from controllers into request service class

--- controllers/hurricanes.js
+++ controllers/hurricanes.js
@@ -1,53 +1,10 @@
-const {MongoClient} = require('mongodb')
-const assert = require('assert')
-const QueryService = require('../services/query')
-
-const url = 'mongodb://localhost:27017/disaster'
+const RequestService = require('../services/request')
 
 module.exports = {
   // URL/api/hurricanes/
   // URL/api/hurricanes?year=2000&month=06&day=23
   index (req, res/*, next*/) {
-    /* Get variables from url */
-    const {
-      year,
-      month,
-      day,
-      yearStart,
-      yearEnd,
-      monthStart,
-      monthEnd,
-      dayStart,
-      dayEnd,
-      limit = 0
-    } = req.query
-
-    const query = QueryService.getQuery(
-      year,
-      month,
-      day,
-      yearStart,
-      yearEnd,
-      monthStart,
-      monthEnd,
-      dayStart,
-      dayEnd
-    )
-
-    if(query === false){
-      res.status(400).send('Bad Request - Did you mix and match selectors?')
-    }else{
-      MongoClient.connect(url, (err, db) => {
-        assert.equal(err, null)
-
-        db.collection('hurricanes').find(query).limit(parseInt(limit))
-          .toArray((err, result) => {
-            assert.equal(err, null)
-
-            db.close()
-            res.json(result)
-          })
-      })
-    }
+    RequestService.set(req, res, 'hurricanes')
+    RequestService.getData()
   }
 }
---
+++ services/request.js
@@ -0,0 +1,58 @@
+const {MongoClient} = require('mongodb')
+const assert = require('assert')
+const QueryService = require('./query')
+
+const url = 'mongodb://localhost:27017/disaster'
+
+class RequestService {
+  set(req, res, collection){
+    this.req = req
+    this.res = res
+    this.collection = collection
+  }
+
+  getData(){
+    const {
+      year,
+      month,
+      day,
+      yearStart,
+      yearEnd,
+      monthStart,
+      monthEnd,
+      dayStart,
+      dayEnd,
+      limit = 0
+    } = this.req.query
+
+    const query = QueryService.getQuery(
+      year,
+      month,
+      day,
+      yearStart,
+      yearEnd,
+      monthStart,
+      monthEnd,
+      dayStart,
+      dayEnd
+    )
+
+    if(query === false){
+      this.res.status(400).send('Bad Request - Did you mix and match selectors?')
+    }else{
+      MongoClient.connect(url, (err, db) => {
+        assert.equal(err, null)
+
+        db.collection(this.collection).find(query).limit(parseInt(limit))
+          .toArray((err, result) => {
+            assert.equal(err, null)
+
+            db.close()
+            this.res.json(result)
+          })
+      })
+    }
+  }
+}
+
+module.exports = new RequestService()
